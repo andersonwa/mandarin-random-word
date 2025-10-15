629bed7dbd8802765417c44e67f80e6af0b0493d
Anderson Ferreira <andersonwebwa@hotmail.com>
2025-10-15 19:34:56 -0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
   const getRandomLine = async () => {
     setShowPinyin(false)
     setShowTranslation(false)
-    await fetch(`/aulas/aula${aula}.txt`)
+    await fetch(`aulas/aula${aula}.txt`)
       .then((res) => res.text())
       .then((text) => {
         const allLines = text
@@ -28,40 +28,42 @@ export default function App() {
   };
 
   return (
-    <div className="flex flex-row items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-6 flex-auto">
-        <h1 className="text-2xl font-bold mb-4">🎲 Random Line Picker</h1>
-        <label>Class Number</label>
-        <input 
-          className="border border-indigo-600 mb-4 p-3 rounded-lg"
-          name="aula" 
-          value={aula} 
-          onChange={(e)=> setAula(e.target.value)}
-        />
-        <button
-          onClick={getRandomLine}
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-700"
-        >
-          Get Random Line
-        </button>
+    <div class="@container">
+      <div className="flex flex-col flex md:flex-row sm:flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center w-full justify-center min-h-screen bg-gray-100 text-center p-6 flex-auto">
+          <h1 className="text-2xl font-bold mb-4">🎲 Random Line Picker</h1>
+          <label>Class Number</label>
+          <input 
+            className="border border-indigo-600 mb-4 p-3 rounded-lg"
+            name="aula" 
+            value={aula} 
+            onChange={(e)=> setAula(e.target.value)}
+          />
+          <button
+            onClick={getRandomLine}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-700"
+          >
+            Get Random Line
+          </button>
 
-        {randomLine && (
-          <div className="mt-6 text-lg text-gray-800">
-            <strong>Word:</strong> {randomLine.split("=")[0]}
-            <p onClick={() => setShowPinyin(!showPinyin)}>
-              <strong>Pinyin:</strong> {showPinyin ? randomLine.split("=")[1] : 'Click to show'}
-            </p>
-            <p onClick={() => setShowTranslation(!showTranslation)}>
-              <strong>Translation:</strong> {showTranslation ? randomLine.split("=")[2] : 'Click to show'}
-            </p>
+          {randomLine && (
+            <div className="mt-6 text-lg text-gray-800">
+              <strong>Word:</strong> {randomLine.split("=")[0]}
+              <p onClick={() => setShowPinyin(!showPinyin)}>
+                <strong>Pinyin:</strong> {showPinyin ? randomLine.split("=")[1] : 'Click to show'}
+              </p>
+              <p onClick={() => setShowTranslation(!showTranslation)}>
+                <strong>Translation:</strong> {showTranslation ? randomLine.split("=")[2] : 'Click to show'}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col items-center w-full justify-center min-h-screen p-6 flex-auto">
+          <div>
+            <ul className="scroll-behavior">
+              {file.map((item, key) => <li key={key}><p>{item}</p></li>)}
+            </ul>
           </div>
-        )}
-      </div>
-      <div className="flex flex-col items-center justify-center min-h-screen p-6 flex-auto">
-        <div>
-          <ul className="scroll-behavior">
-            {file.map((item, key) => <li key={key}><p>{item}</p></li>)}
-          </ul>
         </div>
       </div>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mandarin-random-word',
   plugins: [
     tailwindcss(),
     react(),
